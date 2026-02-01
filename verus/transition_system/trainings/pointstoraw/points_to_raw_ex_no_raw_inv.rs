7f228d3d6d131f82f9fb769335c624e563ac6a38
Takamitsu Yoshii <tyoshii@jaist.ac.jp>
2026-02-01 15:43:30 +0900

    wip

--- verus/transition_system/trainings/pointstoraw/points_to_raw_ex_no_raw_inv.rs
+++ verus/transition_system/trainings/pointstoraw/points_to_raw_ex_no_raw_inv.rs
@@ -18,6 +18,13 @@ pub struct ConsumerState {
     pub grant: Option<raw_ptr::PointsToRaw>,
 }
 
+pub struct GrantState {
+    pub prod_start: nat,
+    pub prod_end: nat,
+    pub cons_start: nat,
+    pub cons_end: nat,
+}
+
 tokenized_state_machine!(PointsToRawExample {
     fields {
         #[sharding(constant)]
@@ -36,13 +43,13 @@ tokenized_state_machine!(PointsToRawExample {
         pub buffer_dealloc: Option<raw_ptr::Dealloc>,
 
         #[sharding(variable)]
-        pub buffer_perm: raw_ptr::PointsToRaw,
-
-        #[sharding(variable)]
         pub producer: ProducerState,
 
         #[sharding(variable)]
         pub consumer: ConsumerState,
+
+        #[sharding(variable)]
+        pub consumer: GrantState,
     }
 
     #[invariant]
@@ -60,14 +67,12 @@ tokenized_state_machine!(PointsToRawExample {
             length: nat,
             base_addr: nat,
             provenance: raw_ptr::Provenance,
-            buffer_perm: raw_ptr::PointsToRaw,
             buffer_dealloc: raw_ptr::Dealloc,
         )
         {
             require(
                 {
                     &&& length > 0
-                    &&& buffer_perm.is_range(base_addr as int, length as int)
                 }
             );
 
@@ -76,7 +81,6 @@ tokenized_state_machine!(PointsToRawExample {
 
             init base_addr = base_addr;
             init provenance = provenance;
-            init buffer_perm = buffer_perm;
             init buffer_dealloc = Some(buffer_dealloc);
             init producer = ProducerState {
                 split: 0,
@@ -87,6 +91,13 @@ tokenized_state_machine!(PointsToRawExample {
                 split: 0,
                 grant: None,
             };
+
+            init grant_state = GrantState {
+                prod_start: 0,
+                prod_end: 0,
+                cons_start: 0,
+                cons_end: 0,
+            }
         }
     }
 
@@ -95,13 +106,11 @@ tokenized_state_machine!(PointsToRawExample {
             require(at > 0 && at < pre.length);
 
             update split = at;
-
-            update buffer_perm = pre.buffer_perm; // dummy
         }
     }
 
     #[inductive(initialize)]
-    fn initialize_inductive(post: Self, length: nat, base_addr: nat, provenance: raw_ptr::Provenance, buffer_perm: raw_ptr::PointsToRaw, buffer_dealloc: raw_ptr::Dealloc) { }
+    fn initialize_inductive(post: Self, length: nat, base_addr: nat, provenance: raw_ptr::Provenance, buffer_dealloc: raw_ptr::Dealloc) { }
 
     #[inductive(do_split)]
     fn do_split_inductive(pre: Self, post: Self, at: nat) {}
@@ -126,16 +135,49 @@ where
     }
 }
 
+impl<Tok> GhostStuff<Tok>
+where
+    Tok: UniqueValueToken<nat>,
+{
+    pub open spec fn wf(self, inst: PointsToRawExample::Instance, cell: PAtomicUsize) -> bool {
+        &&& self.perm@.patomic == cell.id()
+        &&& self.token.instance_id() == inst.id()
+        &&& self.perm@.value as nat == self.token.value()
+    }
+}
+
+pub tracked struct BufferPermission {
+    pub tracked pool: PointsToRaw,
+    pub tracked prod: PointsToRaw,
+    pub tracked cons: PointsToRaw,
+}
+
+pub tracked struct GhostBufferPermission
+{
+    pub tracked perm: PCell<BufferPermission>,
+    pub tracked token: PointsToRawExample::grant_state,
+}
+
+impl GhostBufferPermission
+{
+    pub open spec fn wf(self, inst: PointsToRawExample::Instance, cell: PCell<BufferPermission>) -> bool {
+        &&& self.perm@.id == cell.id()
+        &&& self.token.instance_id() == inst.id()
+        &&& self.perm@.value as nat == self.token.value()
+    }
+}
+
 struct_with_invariants!{
     pub struct ExBuffer {
         length: usize,
         buffer_ptr: *mut u8,
         split: PAtomicUsize,
 
-        buffer_perm_inv: Tracked< Shared<AtomicInvariant<_, PointsToRawExample::buffer_perm, _>> >,
+        points_to_raw_inv: Tracked< Shared<AtomicInvariant<_, GhostBufferPermission, _>> >,
         split_inv: Tracked< Shared<AtomicInvariant<_, GhostStuff<PointsToRawExample::split>, _>> >,
 
         instance: Tracked<PointsToRawExample::Instance>,
+        cell_buffer_permission: Tracked<PCell<BufferPermission>>,
         producer: Tracked<Option<PointsToRawExample::producer>>,
         consumer: Tracked<Option<PointsToRawExample::consumer>>,
     }
@@ -144,14 +186,14 @@ struct_with_invariants!{
         predicate {
             &&& self.instance@.length() == self.length
             &&& self.instance@.length() <= usize::MAX
-            &&& self.split_inv@@.namespace() != self.buffer_perm_inv@@.namespace()
+            &&& self.split_inv@@.namespace() != self.points_to_raw_inv@@.namespace()
         }
 
-        invariant on buffer_perm_inv with (instance)
-            specifically (self.buffer_perm_inv@@)
-            is (v: PointsToRawExample::buffer_perm)
+        invariant on points_to_raw_inv with (instance, cell_buffer_permission)
+            specifically (self.points_to_raw_inv@@)
+            is (v: GhostBufferPermission)
         {
-            &&& v.instance_id() === instance@.id()
+            v.wf(instance@, cell_buffer_permission)
         }
 
         invariant on split_inv with (instance, split)
@@ -176,18 +218,17 @@ impl ExBuffer
             r.consumer@ is Some,
             r.consumer@->Some_0.value().grant is None,
     {
-        let (buffer_ptr, Tracked(buffer_perm), Tracked(buffer_dealloc)) = allocate(length, 1);
+        let (buffer_ptr, Tracked(points_to_raw), Tracked(buffer_dealloc)) = allocate(length, 1);
         let tracked (
             Tracked(instance),
             Tracked(split_token),
-            Tracked(buffer_perm_token),
+            Tracked(grant_state_token),
             Tracked(producer_token),
             Tracked(consumer_token),
         ) = PointsToRawExample::Instance::initialize(
             length as nat,
             buffer_ptr as nat,
             buffer_ptr@.provenance,
-            buffer_perm,
             buffer_dealloc,
             Some(buffer_dealloc),
         );
@@ -195,8 +236,18 @@ impl ExBuffer
         let tracked_inst: Tracked<PointsToRawExample::Instance> = Tracked(instance.clone());
 
         let tr_inst = Tracked(instance);
-        let tracked buffer_perm_inv = AtomicInvariant::new(tr_inst, buffer_perm_token, 0);
-        let tracked buffer_perm_inv = Shared::new(buffer_perm_inv); // Shared は Ghost object を中に入れて、duplicate して &T を取り出すことができる。
+        let tracked points_to_raws = BufferPermission {
+            pool: points_to_raw,
+            prod: PointsToRaw::empty(buffer_ptr@.provenance),
+            cons: PointsToRaw::empty(buffer_ptr@.provenance),
+        };
+        let (points_to_raws_cell, Tracked(points_to_raws_cell_perm)) = PCell<BufferPermission>::new();
+        let tracked ghost_buffer_perm = GhostBufferPermission {
+            perm: points_to_raws_cell_perm,
+            token: grant_state_token,
+        },
+        let tracked points_to_raw_inv = AtomicInvariant::new(tr_inst, buffer_perm_token, 0);
+        let tracked points_to_raw_inv = Shared::new(points_to_raw_inv); // Shared は Ghost object を中に入れて、duplicate して &T を取り出すことができる。
 
         let (split, Tracked(split_perm)) = PAtomicUsize::new(0);
         let tracked gss = GhostStuff { perm: split_perm, token: split_token };
